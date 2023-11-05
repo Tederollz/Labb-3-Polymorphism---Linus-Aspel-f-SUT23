aa83cd84f363568ab5b769dc96d545a995edbf73
Linus Aspelöf <Tederollz@hotmail.com>
2023-11-05 13:59:36 +0100

    Add project files.

---
+++ Labb 3 – Polymorphism - Linus Aspelöf SUT23/Geometri.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3___Polymorphism___Linus_Aspelöf_SUT23
+{
+    public class Geometri
+    {
+        public virtual double Area()
+        {
+            return 0.0;
+        }
+    }
+
+    public class Rektangel : Geometri
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rektangel(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+    }
+
+    public class Fyrkant : Geometri
+    {
+        public double SideLength { get; set; }
+
+        public Fyrkant(double sideLength)
+        {
+            SideLength = sideLength;
+        }
+
+        public override double Area()
+        {
+            return SideLength * SideLength;
+        }
+    }
+
+    public class Cirkel : Geometri
+    {
+        public double Radius { get; set; }
+
+        public Cirkel(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ Labb 3 – Polymorphism - Linus Aspelöf SUT23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// Linus Aspelöf SUT23
+namespace Labb_3___Polymorphism___Linus_Aspelöf_SUT23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Geometri[] geometriskaFigurer = new Geometri[3];
+
+            geometriskaFigurer[0] = new Rektangel(5, 4);
+            geometriskaFigurer[1] = new Fyrkant(3);
+            geometriskaFigurer[2] = new Cirkel(2);
+
+            foreach (var figur in geometriskaFigurer)
+            {
+                Console.WriteLine($"Area: {figur.Area()}");
+            }
+        }
+    }
+}
